3579df9e449769df200c1450de5863b74fcd0dce
Léo Grosjean <LeoH@MacBook-Pro-de-Leopold.local>
2017-01-31 16:48:10 +0100

    contrat avec signature

--- src/ProjectBundle/Controller/OffreDePretController.php
+++ src/ProjectBundle/Controller/OffreDePretController.php
@@ -100,12 +100,16 @@ class OffreDePretController extends Controller
 
 		//TODO : générer le contrat
 
+
+
 		$project->setSommeRecue($project->getSommeRecue()+$offreDePret->getSomme());
 		$offreDePret->setAcceptedByBoth(true);
 		$offreDePret->setDatePret(new \Datetime());
 		$em->flush();
 
-		 return $this->redirectToRoute('view_project', array('id' => $project->getId() ));
+		return $this->redirectToRoute('pdftest',array('id'=> $id));
+
+		//return $this->redirectToRoute('view_project', array('id' => $project->getId() ));
 	}
 
 }
